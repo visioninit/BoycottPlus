07622cdacd94c99c9fed2f8dcb8e2bb0ae00d698
Cem Kocagil <cem.kocagil@gmail.com>
2011-09-02 22:45:37 +0300

    manager window

---
+++ src/chrome/content/manager.js
@@ -0,0 +1,1 @@
+//open("chrome://boycottplus/content/manager.xul", "boycottPlusManagerWindow", "height=450,width=520,resizable=yes,dialog=yes,modal=yes,alwaysRaised=yes,chrome=yes,centerscreen=yes,titlebar=yes")
