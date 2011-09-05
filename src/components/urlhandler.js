d82214a83915ab0d938080f00f07113fc19d780b
Cem Kocagil <cem.kocagil@gmail.com>
2011-09-06 01:51:05 +0300

    .bcp url handler

---
+++ src/components/urlhandler.js
@@ -0,0 +1,48 @@
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+Components.utils.import("chrome://boycottplus/content/jsm/broadcast.jsm");
+
+function TheComponent() { }
+
+TheComponent.prototype = {  
+    classDescription: "BoycottPlus URL handler",  
+    classID:          Components.ID("{e7497047-f186-4ca5-82e0-69abb255c632}"),  
+    contractID:       "@boycottplus.org/urlhandler;1",  
+    QueryInterface: XPCOMUtils.generateQI([Components.interfaces.nsIContentPolicy]),  
+    shouldLoad : function (type, location, origin, context, mimeTypeGuess, extra) {
+        const ACCEPT = Components.interfaces.nsIContentPolicy.ACCEPT;
+        const REJECT_TYPE = Components.interfaces.nsIContentPolicy.REJECT_TYPE;
+        const REJECT_OTHER = Components.interfaces.nsIContentPolicy.REJECT_OTHER;
+        const TYPE_DOCUMENT = Components.interfaces.nsIContentPolicy.TYPE_DOCUMENT;
+        
+        if (type !== TYPE_DOCUMENT) {
+            return ACCEPT;
+        }
+        
+        if (location.scheme === "boycottplus") {
+            Broadcast.broadcast("opened", "URL", location.host + location.path);
+            return REJECT_OTHER;
+        }
+        
+        if ((location.scheme === "http" || location.scheme === "https")
+                && location.path.search(/\.bcp$/) !== -1) {
+            Broadcast.broadcast("opened", "URL", location.host + location.path);
+            return REJECT_OTHER;
+        }
+        
+        return ACCEPT;
+    },
+    shouldProcess : function (type, location, origin, context, mimeType, extra) {
+        const ACCEPT = Components.interfaces.nsIContentPolicy.ACCEPT;
+        
+        return ACCEPT;
+    }
+};
+
+var components = [TheComponent];
+
+if (XPCOMUtils.generateNSGetFactory) {
+    var NSGetFactory = XPCOMUtils.generateNSGetFactory(components);  
+}
+else {
+    var NSGetModule = XPCOMUtils.generateNSGetModule(components);  
+}
