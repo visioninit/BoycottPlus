8ec97b8a1cf05629028d2e09f1d717ba52f81e66
Cem Kocagil <cem.kocagil@gmail.com>
2011-10-30 00:48:27 +0300

    further namespace cleaning

--- src/chrome/content/toolbarButton/button.js
+++ src/chrome/content/toolbarButton/button.js
@@ -1,16 +1,18 @@
 Components.utils.import("resource://boycottplus/modules/boycottplus.jsm");
 
-function onAddClicked () {
-    var url = window.prompt("Enter the Boycott URL:");
-    if (url) {
-        boycottPlus.tools.broadcast("SourceAddedOrUpdated", "event", JSON.stringify(["...", url]));
-        boycottPlus.data.addOrUpdateSource(url);
-    }
-}
+boycottPlus.toolbarButton = {
+    onAddClicked : function () {
+        var url = window.prompt("Enter the Boycott URL:");
+        if (url) {
+            boycottPlus.tools.broadcast("SourceAddedOrUpdated", "event", JSON.stringify(["...", url]));
+            boycottPlus.data.addOrUpdateSource(url);
+        }
+    },
 
-function onRemoveClicked () {
-    var source = document.querySelector("#boycottToolbarDisplayWidget").getSelected();
-    if (source) {
-        boycottPlus.data.removeSource(source);
+    onRemoveClicked: function () {
+        var source = document.querySelector("#boycottToolbarDisplayWidget").getSelected();
+        if (source) {
+            boycottPlus.data.removeSource(source);
+        }
     }
-}
+};
