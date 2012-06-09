de8a4652b6316f50649a56c24441c41017d9ccbc
justin <justin@zdevice-7.(none)>
2012-06-09 00:35:35 -0700

    Updating with latest version of addon

--- firefox/src/defaults/preferences/prefs.js
+++
@@ -1,1 +0,0 @@
-pref("extensions.boycottplus.data", '{ "domainToCompanies" : { }, "tracked" : { } }');
--- src/defaults/preferences/default.js
+++
@@ -1,7 +0,0 @@
-pref("extensions.boycottplus.data", '{ "domainToCompanies" : { }, "tracked" : { } }');
-pref("extensions.boycottplus.just_installed", true);
-pref("extensions.boycottplus.notificationType", "1");
-pref("extensions.boycottplus.updateFrecuency", "2");
-pref("extensions.boycottplus.displayChanges", true);
-
-pref("extensions.boycottplus.post_install_url", "http://www.boycottplus.org");
